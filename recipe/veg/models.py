c07fd9a8d300f7dbf64d9734fddbf559ab86af6f
MANISHKUMAR1234567890 <119800527+MANISHKUMAR1234567890@users.noreply.github.com>
2024-09-24 18:17:00 +0530

    Delete recipe directory

--- recipe/veg/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class veg_recipe(models.Model):
-    recipe_name=models.CharField(max_length=50, default=None)
-    recipe_procedure=models.TextField(max_length=2000)
-    recipe_image=models.ImageField(upload_to='media')
